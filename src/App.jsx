c46c9ff9df50efc76f12c19b2e482c1a2bc94554
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-10-20 21:06:03 +0600

    Delete a coffee from the server and database with swal alert

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useLoaderData } from 'react-router-dom'
+import CoffeeCard from './components/CoffeeCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const coffees = useLoaderData()
+console.log(coffees);
   return (
     <>
-      <h1>Home</h1>
+     <div className='container mx-auto'>
+     <h1 className='text-center text-5xl text-purple-600 py-8 mb-12'>Our Popular products: {coffees.length}</h1>
+     <div className='grid md:grid-cols-2 gap-4'>
+     {coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)}
+     </div>
+     </div>
     </>
   )
 }
---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,58 @@
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee }) => {
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
+  const handleDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your Coffee has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+  return (
+    <div>
+      <div className="card card-side bg-base-100 shadow-xl my-6">
+        <figure>
+          <img className="w-[100px] my-8 mx-12" src={photo} alt="Movie" />
+        </figure>
+        <div className="flex justify-between w-full pl-4 pr-4">
+          <div>
+            <h2 className="card-title">Name: {name}</h2>
+            <p>{quantity}</p>
+            <p>{supplier}</p>
+            <p>{taste}</p>
+          </div>
+          <div className="card-actions justify-end">
+            <div className="btn-group btn-group-vertical mt-4 space-y-4">
+              <button className="btn btn-active ">View</button>
+              <button className="btn">Edit</button>
+              <button onClick={() => handleDelete(_id)} className="btn">
+                X
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
